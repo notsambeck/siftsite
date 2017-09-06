d9b00291cc9ae6c32369f65e8c76413545325505
Sam Beck <notsambeck@gmail.com>
2017-09-06 16:53:34 -0700

    stat_root = media in settings.py

--- siftsite/settings.py
+++ siftsite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 INSTALLED_APPS = [
