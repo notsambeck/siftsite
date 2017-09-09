5968b2c1b8b942d8c2a607eaebe5782e17893250
Sam Beck <notsambeck@gmail.com>
2017-09-09 14:17:00 -0700

    trying to add media to staticfiles_dirs

--- siftsite/settings.py
+++ siftsite/settings.py
@@ -42,6 +42,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
 
 
 INSTALLED_APPS = [
