d068978c48fcdaf3a2d8b57113b45075cdfbebf6
Sam Beck <notsambeck@gmail.com>
2017-10-08 13:43:52 -0700

    changed it back

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Setup file for siftsite. x
+    Setup file for siftsite.
 
     This file was generated with PyScaffold 2.5.7, a tool that easily
     puts up a scaffold for your new Python project. Learn more under:
